f350e4b4a8600dcc2b08cea84ad7e7ce05fa5ecd
Tero Keränen <tero-keranen@hotmail.com>
2024-03-23 09:57:06 +0200

    testing

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -88,8 +88,8 @@ router.post("/signin", async (req, res) => {
     const refreshToken = generateRefreshToken(user);
     user.refreshToken = refreshToken
     await user.save();
-    console.log("SIGNIN", user)
-    res.send({ accessToken, refreshToken, user: { email: user.email, _id: user.id, role: user.role, isVerified: user.isVerified } });
+    
+    res.send({ accessToken, refreshToken, user: { email: user.email, _id: user._id, role: user.role, isVerified: user.isVerified } });
     // res.send({ token, user: {email:user.email,_id:user.id, role: user.role, isVerified: user.isVerified} }); //MUUTOS
   } catch (err) {
     return res.status(422).send({ error: "invalid password or email" });
