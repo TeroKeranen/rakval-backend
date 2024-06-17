c418f31287db842e6d716ad42d04cc5107a17649
Tero Keränen <tero-keranen@hotmail.com>
2024-06-17 21:31:48 +0300

    testing

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -454,6 +454,7 @@ router.get('/aws-url', requierAuth, async (req,res) => {
 router.delete('/deleteAccount', requierAuth, async (req,res) => {
   const userId = req.user._id;
 
+  console.log("user id", userId);
   try {
     const user = await User.findById(userId)
 
