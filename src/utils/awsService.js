081803790b83d7094a65658ab58a074617148e53
Tero Keränen <tero-keranen@hotmail.com>
2024-08-31 20:00:48 +0300

    fix awsService bug

--- src/utils/awsService.js
+++ src/utils/awsService.js
@@ -1,6 +1,6 @@
 
 require("dotenv").config();
-const {S3Client, GetObjectCommand, S3Client} = require("@aws-sdk/client-s3");
+const {S3Client, GetObjectCommand} = require("@aws-sdk/client-s3");
 const {getSignedUrl} = require("@aws-sdk/s3-request-presigner");
 const { Credentials } = require("aws-sdk");
 
