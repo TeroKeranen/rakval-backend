a148533b5200f6092209377d864455b949167bfa
Tero Keränen <tero-keranen@hotmail.com>
2024-09-05 21:25:00 +0300

    new route for updating user role

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -545,4 +545,31 @@ router.post('/reset-password/:token', async (req,res) => {
   res.status(200).json({success: true, info: 'password changed successfully'})
 })
 
+// Muokataan käyttäjän roolia
+router.put('/update-role/:id', requierAuth, async (req,res) => {
+  const {id} = req.params;
+  const {role} = req.body;
+
+  if (!role) {
+    return res.status(400).json({ success: false, message: 'Role is required' });
+ 
+  }
+
+  try {
+    
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    user.role = role;
+    await user.save();
+
+    return res.json({ success: true, message: 'User role updated successfully', user });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+})
+
 module.exports = router;
